4e701242380a39a73c8c8c20d0e41bf1be94d871
reyzonek <pawel1221@gmail.com>
2021-12-27 11:32:56 +0100

    js version

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,16 @@
-function Fibonacci(number){  
-    result = (1 / Math.sqrt(5)) * Math.pow(((1 + Math.sqrt(5)) / 2), number) - (1 / Math.sqrt(5)) * Math.pow(((1 - Math.sqrt(5)) / 2), number)     
-} 
 
-console.log(Fibonacci(6));
+const number = parseInt(prompt('Podaj ktory element ciagu fibonacciego chcesz otrzymac: '))
+
+ 
+  //  result = (1 / Math.sqrt(5)) * Math.pow(((1 + Math.sqrt(5)) / 2), number) - (1 / Math.sqrt(5)) * Math.pow(((1 - Math.sqrt(5)) / 2), number)     
+let first = 0;
+let second = 1;
+
+for (let i = 1; i <= number; i++) {
+  console.log(first);
+  next = first + second;
+  first = second;
+  second = next;
+}
+console.log('Ten element ciagu wynosi: ')
+console.log(first);
